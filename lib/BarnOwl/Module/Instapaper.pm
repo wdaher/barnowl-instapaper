5b48f6cf0d65b97174c06da382494d42e308a780
Waseem Daher <wdaher@mit.edu>
2011-10-11 22:28:57 -0400

    First functional version.

--- lib/BarnOwl/Module/Instapaper.pm
+++ lib/BarnOwl/Module/Instapaper.pm
@@ -15,27 +15,77 @@ package BarnOwl::Module::Instapaper;
 
 our $VERSION = 0.1;
 
+require URI::Find::Schemeless;
+use LWP::UserAgent;
+use JSON;
+use BarnOwl;
+
+my $conffile = BarnOwl::get_config_dir() . "/instapaper";
+open(my $fh, "<", "$conffile") || fail("Unable to read $conffile");
+my $cfg = do {local $/; <$fh>};
+close($fh);
+eval {
+    $cfg = from_json($cfg);
+};
+if($@) {
+    fail("Unable to parse ~/.owl/instapaper: $@");
+}
+my $username = $cfg->{user};
+my $password = $cfg->{password};
+
+my $ua = LWP::UserAgent->new;
+$ua->agent("BarnOwl-Instapaper/$VERSION");
+
 sub cmd_readlater {
+    ## TODO: maybe allow you to manually specify a url as an argument?
     my $cmd = shift;
     my $args = join(" ", @_);
+    my $m = BarnOwl::getcurmsg();
+    my $body = $m->body;
+
+    ## Borrowed from http://perladvent.pm.org/2002/1st/
+    # a list of the urls
+    my @urls;
+    my $actually_found_anything = 0;
+    my $finder = URI::Find::Schemeless->new(sub {
+	my $uri    = shift;  # object representing the url
+	my $string = shift;  # text that was in the url
+	# remember the $uri's address by adding it to @urls which
+	# we can see from within this sub
+	push @urls, $uri->abs;
+	$actually_found_anything = 1;
+	# return the original text back to leave the text
+	# we were searching unaltered
+	return $string;
+     });
+
+    # and process the text through that finder
+    $finder->find(\$body);
+
+    if ( $actually_found_anything ) {
+	foreach my $url (@urls)
+	{
+	    process_url($url);
+	}
+    } else {
+	BarnOwl::message("Sorry, homie, we didn't actually find any URLs in your message.");
+    }
 }
 
-sub trim {
-    my $s = shift;
-    $s =~ s/^\s+//;
-    $s =~ s/\s+$//;
-    return $s;
-}
+sub process_url {
+    my $url = shift;
+    BarnOwl::message("Trying to add $url...");
 
-sub colorize {
-    my $text = shift;
-    my $num = shift;
-    my $color;
-    if($num <= 3) { $color = "green" }
-    elsif($num <= 6) {$color = "yellow"}
-    else {$color = "red";}
+    my $req = HTTP::Request->new(POST => 'https://www.instapaper.com/api/add');
+    $req->content_type('application/x-www-form-urlencoded');
+    $req->content("username=$username&password=$password&url=$url");
 
-    return '@<@color(' . $color . ")$text>";
+    my $res = $ua->request($req);
+    if ( $res->is_success ) {
+	BarnOwl::message("Added $url to Instapaper. Enjoy!");
+    } else {
+	BarnOwl::message("Sorry, something went wrong: " . $res->status_line);
+    }
 }
 
 BarnOwl::new_command(readlater => \&cmd_readlater, {
